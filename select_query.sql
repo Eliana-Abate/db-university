30048570a9c067b1011517791e416c1249d69f69
Eliana-Abate <eliana.abate@outlook.it>
2021-09-28 15:46:54 +0200

    corrected date in query n.3

--- select_query.sql
+++ select_query.sql
@@ -1,4 +1,4 @@
-/* 1. Selezionare tutti gli studenti nati nel 1990 (160) */
+/* 1. Selezionare tutti gli studenti nati nel 1990 */
 
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students` 
@@ -14,10 +14,12 @@ WHERE `cfu` > 10;
 
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students` 
-WHERE `date_of_birth` <= '1991-09-28'
+WHERE `date_of_birth` <= '1990-09-28'
 ORDER BY `date_of_birth` DESC;
 
 /* 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea */
+
+
 /* 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020  */
 /* 6. Selezionare tutti i corsi di laurea magistrale */
 /* 7. Da quanti dipartimenti è composta l'università? */
