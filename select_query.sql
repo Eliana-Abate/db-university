623139848a0fbf3c3d96f7a2a68862c2470e3e95
Eliana-Abate <eliana.abate@outlook.it>
2021-09-28 17:00:47 +0200

    corrected line 5

--- select_query.sql
+++ select_query.sql
@@ -2,7 +2,7 @@
 
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students` 
-WHERE YEAR(`date_of_birth`) = '1990';
+WHERE YEAR(`date_of_birth`) = 1990;
 
 /* 2.  Selezionare tutti i corsi che valgono più di 10 crediti */
 
