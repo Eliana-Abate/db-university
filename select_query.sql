fc8abf7acbb4c6b6d3bd7ba448133885d3dde03d
Eliana-Abate <eliana.abate@outlook.it>
2021-09-28 15:58:36 +0200

    added 5th

--- select_query.sql
+++ select_query.sql
@@ -26,6 +26,12 @@ WHERE `period` = 'I semestre' AND `year` = '1';
 
 
 /* 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020  */
+
+SELECT `course_id`, `date`, `hour`
+FROM `exams` 
+WHERE `date` = '2020-06-20' AND `hour` > '14:00'
+ORDER BY `hour` ASC;
+
 /* 6. Selezionare tutti i corsi di laurea magistrale */
 /* 7. Da quanti dipartimenti è composta l'università? */
 /* 8. Quanti sono gli insegnanti che non hanno un numero di telefono? */
