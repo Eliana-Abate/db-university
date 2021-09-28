7d259b8a3ecdb3ebefc9bd68d6e874b6e42fd0b0
Eliana-Abate <eliana.abate@outlook.it>
2021-09-28 16:04:17 +0200

    added name in 6th query

--- select_query.sql
+++ select_query.sql
@@ -34,10 +34,12 @@ ORDER BY `hour` ASC;
 
 /* 6. Selezionare tutti i corsi di laurea magistrale */
 
-SELECT `level` 
+SELECT  `name`, `level` 
 FROM `degrees` 
 WHERE `level` = 'magistrale';
 
 /* 7. Da quanti dipartimenti è composta l'università? */
+
+
 
 /* 8. Quanti sono gli insegnanti che non hanno un numero di telefono? */
